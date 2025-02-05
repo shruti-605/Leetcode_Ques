17c33823dc0f96f7f16dda898a489ca374da8f73
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2025-02-05 15:49:36 +0530

    Create BalancedTree110.cpp

---
+++ BalancedTree.cpp
@@ -0,0 +1,19 @@
+class Solution {    
+public:
+    int find(TreeNode* node){
+        if(node==NULL){
+            return true;// height of tree is 0
+        }
+        int left=find(node->left);//leftsubtree height
+        int right=find(node->right);//rightsubtree height
+
+        if(abs(left-right)>1) return -1;// diff of height of left and right subtree less than 1 if more than 1 then unbalanced
+        if(left==-1 || right==-1 ) return -1; //unbalanced tree
+
+        return 1 + max(left,right);
+    }
+    bool isBalanced(TreeNode* root) {
+       int val=find(root);//check teh height and balanced tree
+       return (val!=-1);//-1 the tree is unbalanced 
+    }
+};
