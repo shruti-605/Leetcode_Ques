421fa049b82240f3b5d0ff34f098f438598b3a13
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2026-01-16 13:36:54 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (81.04%) - LeetSync

--- 2650-split-with-minimum-sum/split-with-minimum-sum.cpp
+++ 2650-split-with-minimum-sum/split-with-minimum-sum.cpp
@@ -4,14 +4,14 @@ public:
         //convert number to string
         string s=to_string(num);
         //sort
-        sort(s.begin(),s.end());
+        sort(s.begin(),s.end());//2345
 
         string n1="" , n2="";
         for(int i=0;i<s.length();i++){
-            if(i%2==0){
-                n1+=s[i];
+            if(i%2==0){//
+                n1+=s[i];//24
             }else{
-                n2+=s[i];
+                n2+=s[i];//35
 
             }
                 
