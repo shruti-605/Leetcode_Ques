e446dbf7d95324580c9895753a4024b8faeaf65c
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2026-01-18 21:22:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (90.97%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int evenSum=0,oddSum=0;
+
+        for(int i=0;i<num.size();i++){
+            int digit=num[i]-'0';//convert character to integet
+
+            if(i%2==0){
+                evenSum+=digit;
+            }else{
+                oddSum+=digit;
+            }
+        }
+        return evenSum==oddSum;
+    }
+};
