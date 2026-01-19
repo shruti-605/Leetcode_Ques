3a653829097fea2d727e91bf128dfce409bca9e9
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2026-01-19 13:29:38 +0530

    Time: 4 ms (18.69%) | Memory: 14.8 MB (21.35%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int>freq;
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
+        }
+        vector<int>ans;
+        for(auto it:freq){
+            if(it.second==1){
+                ans.push_back(it.first);
+            }
+        }
+        
+        return ans;
+    }
+};
