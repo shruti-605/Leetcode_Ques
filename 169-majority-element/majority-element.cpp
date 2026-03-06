98ea99107599ff23dbefe58359672baf2fa1a59f
shruti-605 <114794288+shruti-605@users.noreply.github.com>
2026-03-06 11:23:41 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.69%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,19 +1,30 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //boyer moore algo
+        unordered_map<int,int>mp;
         int n=nums.size();
-        int freq=0,ans=0;
-        for(int i=0;i<n;i++){
-            if(freq==0){
-                ans=nums[i];
-            }
-            if(ans==nums[i]){
-                freq++;
-            }else{
-                freq--;
-            }
+        for(int x:nums){
+         mp[x]++;
+        
+        if(mp[x]>n/2){
+            return x;
         }
-        return ans;
+        }
+        return -1;
+        //boyer moore algo
+        // int n=nums.size();
+        // int freq=0,ans=0;
+        // for(int i=0;i<n;i++){
+        //     if(freq==0){//3==0,freq==1,freq==2
+        //         ans=nums[i];//ans=nums[0]=3
+        //     }
+        //     if(ans==nums[i]){3==3,3==3,3==4
+        //     //freq=1
+        //         freq++;
+        //     }else{
+        //         freq--;//freq=1
+        //     }
+        // }
+        // return ans;
     }
 };
