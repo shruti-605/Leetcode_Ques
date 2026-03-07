40d3b6e4e5184e3d484c2a32129abe39e85c111d
Shruti Kumari <114794288+shruti-605@users.noreply.github.com>
2026-03-07 16:36:37 +0530

    Time: 393 ms (58.01%) | Memory: 0B (100.00%) - LeetSync

--- 1670-patients-with-a-condition/patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/patients-with-a-condition.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
--- select patient_id, patient_name,conditions
--- from Patients
--- where conditions like 'DIAB1%' 
--- or conditions like '%DIAB1%';
+select patient_id, patient_name,conditions
+from Patients
+where conditions like 'DIAB1%' 
+or conditions like '% DIAB1%';
 
-SELECT patient_id, patient_name, conditions
-FROM Patients
-WHERE conditions LIKE 'DIAB1%'
-   OR conditions LIKE '% DIAB1%';
+-- SELECT patient_id, patient_name, conditions
+-- FROM Patients
+-- WHERE conditions LIKE 'DIAB1%'
+--    OR conditions LIKE '% DIAB1%';
