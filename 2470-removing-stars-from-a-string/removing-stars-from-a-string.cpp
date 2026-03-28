124446c6a5ffa2f7b5336764e5b644fea29036cf
Shruti Kumari <114794288+shruti-605@users.noreply.github.com>
2026-03-28 16:49:52 +0530

    Time: 20 ms (47.34%) | Memory: 28.4 MB (73.16%) - LeetSync

---
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        string ans="";
+        for(char c:s){
+            if(c=='*'){
+                if(!ans.empty()){
+                    ans.pop_back();
+                }
+                }else{
+                    ans.push_back(c);
+                }
+            }
+        return ans;
+    }
+};
