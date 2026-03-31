fa5b7540319d255a33d1c540c76fe810b0a17e59
Shruti Kumari <114794288+shruti-605@users.noreply.github.com>
2026-03-31 15:56:47 +0530

    Time: 17 ms (48.54%) | Memory: 53.1 MB (80.54%) - LeetSync

---
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int matrixSum(vector<vector<int>>& nums) {
+        int n=nums.size();        
+        int m=nums[0].size();
+
+        for(int i=0;i<n;i++){
+        sort(nums[i].begin(),nums[i].end());
+        }
+        int ans=0;
+        for(int j=m-1;j>=0;j--){
+            int maxi=0;
+            for(int i=0;i<n;i++){
+                maxi=max(maxi,nums[i][j]);
+            }
+            ans+=maxi;
+        }
+        return ans;
+    }
+};
